20ec1dab86dc6252c1a378b811f443154cde3021
William Li <williamli@bbi.io>
2019-05-24 19:43:32 +0800

    echo path

--- index.py
+++ index.py
@@ -6,4 +6,4 @@ css = '<link rel="stylesheet" href="/css/style.css" />'
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Visit the <a href='./about'>about</a> page or view the <a href='%s'>source code</a>.</p>" % (css, source), mimetype='text/html')
+    return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Path: /%s</p>" % (css, path), mimetype='text/html')
