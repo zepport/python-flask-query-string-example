6c5876079065ee1ecc9668cd0ba66be9f075ed36
William Li <williamli@bbi.io>
2019-06-10 12:12:36 +0800

    display query string args

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, __version__
+from flask import Flask, Response, __version__, request
 app = Flask(__name__)
 source = 'https://github.com/zeit/now-examples/tree/master/python-flask'
 css = '<link rel="stylesheet" href="/css/style.css" />'
@@ -6,4 +6,4 @@ css = '<link rel="stylesheet" href="/css/style.css" />'
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Path: /%s</p>" % (css, path), mimetype='text/html')
+    return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Path: /%s</p><p>Query String (args): %s</p>" % (css, path, request.args), mimetype='text/html')
