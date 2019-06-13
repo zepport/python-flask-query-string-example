5e2b4368ad59c4f239ceab27c285bb91d124718a
William Li <williamli@bbi.io>
2019-06-13 22:05:45 +0800

    update script to test for "code" in query string

--- index.py
+++ index.py
@@ -6,4 +6,7 @@ css = '<link rel="stylesheet" href="/css/style.css" />'
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    code = request.args.get("code")
+    if code:
+      return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Path: /%s</p><p>Code in Query String: %s</p>" % (css, path, request.args.get("code")), mimetype='text/html')
     return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Path: /%s</p><p>Query String (args): %s</p>" % (css, path, request.args), mimetype='text/html')
